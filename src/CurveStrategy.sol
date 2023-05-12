7a741e898605aafeec05055872ca632997612afc
clement-ux <clemmoller@gmail.com>
2023-05-12 10:06:19 +0200

    fix: handle deposit in convex frax

--- src/CurveStrategy.sol
+++ src/CurveStrategy.sol
@@ -10,7 +10,9 @@ import {Optimizor} from "src/Optimizor.sol";
 import {ConvexMapper} from "src/ConvexMapper.sol";
 
 import {ILocker} from "src/interfaces/ILocker.sol";
+import {IBoosterConvexFrax} from "src/interfaces/IBoosterConvexFrax.sol";
 import {IBoosterConvexCurve} from "src/interfaces/IBoosterConvexCurve.sol";
+import {IStakingProxyConvex} from "src/interfaces/IStakingProxyConvex.sol";
 
 contract CurveStrategy is Auth {
     using SafeTransferLib for ERC20;
@@ -19,7 +21,13 @@ contract CurveStrategy is Auth {
 
     Optimizor public optimizor;
     ConvexMapper public convexMapper;
+    IBoosterConvexFrax public boosterConvexFrax; // Convex Frax booster
+
+    uint256 public lockingIntervalSec = 7 days; // 7 days
+
     mapping(address => address) public gauges; // token address --> gauge address
+    mapping(uint256 => address) public vaults; // pid --> vault address
+    mapping(address => bytes32) public kekIds; // vault address --> kekId
 
     error ADDRESS_NULL();
     error DEPOSIT_FAIL();
@@ -27,6 +35,7 @@ contract CurveStrategy is Auth {
     constructor(Authority _authority) Auth(msg.sender, _authority) {
         optimizor = new Optimizor();
         convexMapper = new ConvexMapper();
+        boosterConvexFrax = IBoosterConvexFrax(0x569f5B842B5006eC17Be02B8b94510BA8e79FbCa);
     }
 
     function deposit(address token, uint256 amount) external {
@@ -58,11 +67,27 @@ contract CurveStrategy is Auth {
             require(success, "Deposit failed!");
         }
 
-        // Deposit on Convex
+        // Deposit all the remaining on Convex
         if (amount > 0) {
             if (isOnConvexFrax) {
                 // Deposit on ConvexFrax
+                if (vaults[pid] == address(0)) {
+                    // Create personal vault if not exist
+                    vaults[pid] = boosterConvexFrax.createVault(pid);
+                }
+                // Safe approve lp token to personal vault
+                ERC20(token).safeApprove(vaults[pid], amount);
+
+                if (kekIds[vaults[pid]] == bytes32(0)) {
+                    // If no kekId, stake locked curve lp
+                    kekIds[vaults[pid]] =
+                        IStakingProxyConvex(vaults[pid]).stakeLockedCurveLp(amount, lockingIntervalSec);
+                } else {
+                    // Else lock additional curve lp
+                    IStakingProxyConvex(vaults[pid]).lockAdditionalCurveLp(kekIds[vaults[pid]], amount);
+                }
             } else {
+                // Deposit on ConvexCurve
                 // Cache Convex Curve booster address
                 IBoosterConvexCurve convex = convexMapper.boosterConvexCurve();
                 // Safe approve lp token to convex curve booster
---
+++ src/interfaces/IStakingProxyConvex.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity 0.8.19;
+
+interface IStakingProxyConvex {
+    function stakeLockedCurveLp(uint256 _liquidity, uint256 _secs) external returns (bytes32 kek_id); 
+
+    function lockAdditionalCurveLp(bytes32 _kek_id, uint256 _addl_liq) external;
+}
