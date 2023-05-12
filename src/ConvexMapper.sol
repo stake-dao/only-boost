24a906eb84973f3ce158eb9c7320d75bf55eafd9
clement-ux <clemmoller@gmail.com>
2023-05-12 12:02:33 +0200

    fix: length of last pids count for convex

--- src/ConvexMapper.sol
+++ src/ConvexMapper.sol
@@ -9,8 +9,8 @@ contract ConvexMapper {
     IBoosterConvexCurve public boosterConvexCurve; // Convex booster
     IPoolRegistryConvexFrax public poolRegistryConvexFrax; // ConvexFrax pool Registry
 
-    uint256 public pidsCountConvexFrax; // Number of pools on ConvexFrax
-    uint256 public pidsCountConvexCurve; // Number of pools on ConvexCurve
+    uint256 public lastPidsCountConvexFrax; // Number of pools on ConvexFrax
+    uint256 public lastPidsCountConvexCurve; // Number of pools on ConvexCurve
 
     mapping(address => uint256) public pidsConvexFrax; // lpToken address --> pool ids from convexFrax
     mapping(address => uint256) public pidsConvexCurve; // lpToken address --> pool ids from convexCurve
@@ -18,14 +18,13 @@ contract ConvexMapper {
     constructor() {
         boosterConvexCurve = IBoosterConvexCurve(0xF403C135812408BFbE8713b5A23a04b3D48AAE31);
         poolRegistryConvexFrax = IPoolRegistryConvexFrax(0x41a5881c17185383e19Df6FA4EC158a6F4851A69);
+        setAllPidsOnConvexFraxOptimized();
+        setAllPidsOnConvexCurveOptimized();
     }
 
     // === Convex Curve === //
     function setPidOnConvexCurve(uint256 index) public {
         (address lpToken,,,,,) = boosterConvexCurve.poolInfo(index);
-
-        // If the lpToken is not in the list, add it
-        if (pidsConvexFrax[lpToken] == 0) ++pidsCountConvexCurve;
 
         // Set the lpToken address
         pidsConvexCurve[lpToken] = index;
@@ -36,20 +35,20 @@ contract ConvexMapper {
         uint256 len = boosterConvexCurve.poolLength();
 
         // If the length is the same, no need to update
-        if (pidsCountConvexCurve == len) return;
+        if (lastPidsCountConvexCurve == len) return;
 
         // If the length is smaller, update pids mapping
-        for (uint256 i = pidsCountConvexCurve; i < len; ++i) {
+        for (uint256 i = lastPidsCountConvexCurve; i < len; ++i) {
             setPidOnConvexCurve(i);
         }
+
+        // Update the last length
+        lastPidsCountConvexCurve = len;
     }
 
     // === Convex Frax === //
     function setPidOnConvexFrax(uint256 index) public {
         (,, address lpToken,,) = poolRegistryConvexFrax.poolInfo(index);
-
-        // If the lpToken is not in the list, add it
-        if (pidsConvexFrax[lpToken] == 0) ++pidsCountConvexFrax;
 
         // Set the lpToken address
         pidsConvexFrax[lpToken] = index;
@@ -60,12 +59,15 @@ contract ConvexMapper {
         uint256 len = poolRegistryConvexFrax.poolLength();
 
         // If the length is the same, no need to update
-        if (pidsCountConvexFrax == len) return;
+        if (lastPidsCountConvexFrax == len) return;
 
         // If the length is smaller, update pids mapping
-        for (uint256 i = pidsCountConvexFrax; i < len; ++i) {
+        for (uint256 i = lastPidsCountConvexFrax; i < len; ++i) {
             setPidOnConvexFrax(i);
         }
+
+        // Update the last length
+        lastPidsCountConvexFrax = len;
     }
 
     function getPid(address token) public returns (bool, uint256) {
