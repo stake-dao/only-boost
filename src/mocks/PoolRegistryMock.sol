e8b27e90d590c5efbe4fe28c47def253b0b48ea7
clement-ux <clemmoller@gmail.com>
2023-06-14 13:29:30 +0200

    add: WIP prepare setup for invariant testing

--- src/mocks/PoolRegistryMock.sol
+++ src/mocks/PoolRegistryMock.sol
@@ -3,7 +3,21 @@
 pragma solidity 0.8.19;
 
 contract PoolRegistryMock {
-    function poolLength() external pure returns (uint256) {
-        return 0;
+    struct PoolInfo {
+        address implementation;
+        address stakingAddress;
+        address stakingToken;
+        address rewardsAddress;
+        uint8 active;
+    }
+
+    PoolInfo[] public poolInfo;
+
+    function poolLength() external view returns (uint256) {
+        return poolInfo.length;
+    }
+
+    function addPool(address stakingAddress, address stakingToken) external {
+        poolInfo.push(PoolInfo(address(0), stakingAddress, stakingToken, address(0), 1));
     }
 }
--- test/invariants/CurveStrategyInvariants.t.sol
+++ test/invariants/CurveStrategyInvariants.t.sol
@@ -8,6 +8,8 @@ import {Authority} from "solmate/auth/Auth.sol";
 import {RolesAuthority} from "solmate/auth/authorities/RolesAuthority.sol";
 import {MockERC20} from "solmate/test/utils/mocks/MockERC20.sol";
 
+import {LibString} from "solady/src/utils/LibString.sol";
+
 import {Optimizor} from "src/Optimizor.sol";
 import {BaseFallback} from "src/BaseFallback.sol";
 import {CurveStrategy} from "src/CurveStrategy.sol";
@@ -16,11 +18,15 @@ import {FallbackConvexFrax} from "src/FallbackConvexFrax.sol";
 import {FallbackConvexCurve} from "src/FallbackConvexCurve.sol";
 
 import {PoolRegistryMock} from "src/mocks/PoolRegistryMock.sol";
+import {BaseRewardPoolMock} from "src/mocks/BaseRewardPoolMock.sol";
+import {LiquidityGaugeMock} from "src/mocks/LiquidityGaugeMock.sol";
 import {BoosterConvexFraxMock} from "src/mocks/BoosterConvexFraxMock.sol";
 import {BoosterConvexCurveMock} from "src/mocks/BoosterConvexCurveMock.sol";
 
 contract CurveStrategyInvariantsTest is Test {
     using stdStorage for StdStorage;
+
+    address public immutable STK_ALUSD_FRAXBP = makeAddr("STK_ALUSD_FRAXBP");
 
     MockERC20 public immutable CRV = new MockERC20("Curve DAO Token", "CRV", 18);
     MockERC20 public immutable CRV3 = new MockERC20("3 Pool", "CRV3", 18);
@@ -38,39 +44,103 @@ contract CurveStrategyInvariantsTest is Test {
     FallbackConvexCurve public fallbackConvexCurve;
 
     function setUp() public {
-        _labelTokens();
-        vm.mockCall(
-            address(0x78FA799DFf1eC4F2974a891A176d5a9b878868A9),
-            abi.encodeWithSignature("boosterConvexFrax()"),
-            abi.encode(address(boosterConvexFraxMock))
-        );/*
-        vm.mockCall(
-            address(0x78FA799DFf1eC4F2974a891A176d5a9b878868A9),
-            abi.encodeWithSignature("poolRegistryConvexFrax()"),
-            abi.encode(address(poolRegistryMock))
-        );
-        vm.mockCall(
-            address(0x8F2a8FbCCc162fc96Fe5dEff18960eB878Bb738B),
-            abi.encodeWithSignature("boosterConvexCurve()"),
-            abi.encode(address(boosterConvexCurveMock))
-        );*/
+        vm.mockCall(STK_ALUSD_FRAXBP, abi.encodeWithSignature("curveToken()"), abi.encode(address(ALUSD_FRAXBP)));
         rolesAuthority = new RolesAuthority(address(this), Authority(address(0)));
         curveStrategy = new CurveStrategy(address(this), rolesAuthority);
-        optimizor = curveStrategy.optimizor();
-        fallbackConvexFrax = optimizor.fallbackConvexFrax();
-        fallbackConvexCurve = optimizor.fallbackConvexCurve();
-        //stdstore.target(address(fallbackConvexFrax)).sig("boosterConvexFrax()").checked_write(address(0x111));
+        _deployFallbacksModified();
+        optimizor = new Optimizor(
+            address(this), 
+            rolesAuthority, 
+            address(curveStrategy), 
+            address(fallbackConvexCurve), 
+            address(fallbackConvexFrax)
+        );
+
+        _labels();
     }
 
     function test_Nothing() public {
         assertTrue(true);
-        console.log(address(fallbackConvexFrax.boosterConvexFrax()));
+        console.log(address(fallbackConvexFrax.BOOSTER()));
+        console.log(address(fallbackConvexFrax.POOL_REGISTRY()));
     }
 
-    function _labelTokens() internal {
+    function _labels() internal {
+        // Label tokens
         vm.label(address(CRV), "CRV");
         vm.label(address(CRV3), "CRV3");
         vm.label(address(ALUSD_FRAXBP), "ALUSD_FRAXBP");
         vm.label(address(STETH_ETH), "STETH_ETH");
+
+        // Label mocks contracts
+        vm.label(address(poolRegistryMock), "poolRegistryMock");
+        vm.label(address(boosterConvexFraxMock), "boosterConvexFraxMock");
+        vm.label(address(boosterConvexCurveMock), "boosterConvexCurveMock");
+
+        // Label contracts
+        vm.label(address(optimizor), "optimizor");
+        vm.label(address(curveStrategy), "curveStrategy");
+        vm.label(address(rolesAuthority), "rolesAuthority");
+        vm.label(address(fallbackConvexFrax), "fallbackConvexFrax");
+        vm.label(address(fallbackConvexCurve), "fallbackConvexCurve");
+    }
+
+    function _deployBytecode(bytes memory bytecode, bytes memory args) private returns (address deployed) {
+        bytecode = abi.encodePacked(bytecode, args);
+        assembly {
+            deployed := create(0, add(bytecode, 0x20), mload(bytecode))
+        }
+        require(deployed != address(0), "DEPLOYMENT_FAILED");
+    }
+
+    function _deployFallbacksModified() internal {
+        // --- Convex Curve --- //
+        // Initialize booster
+        _initializeBoosterConvexCurve();
+
+        // Change address of booster
+        bytes memory newBytesCode = bytes(
+            LibString.replace(
+                string(abi.encodePacked(type(FallbackConvexCurve).creationCode)),
+                string(abi.encodePacked(0xF403C135812408BFbE8713b5A23a04b3D48AAE31)),
+                string(abi.encodePacked(address(boosterConvexCurveMock)))
+            )
+        );
+
+        // Deploy new contract using modified bytecode
+        fallbackConvexCurve = FallbackConvexCurve(
+            _deployBytecode(newBytesCode, abi.encode(address(this), rolesAuthority, address(curveStrategy)))
+        );
+
+        // --- Convex Frax --- //
+        // Change address of booster
+        newBytesCode = bytes(
+            LibString.replace(
+                string(abi.encodePacked(type(FallbackConvexFrax).creationCode)),
+                string(abi.encodePacked(0x569f5B842B5006eC17Be02B8b94510BA8e79FbCa)),
+                string(abi.encodePacked(address(boosterConvexFraxMock)))
+            )
+        );
+
+        // Change address of pool registry
+        newBytesCode = bytes(
+            LibString.replace(
+                string(newBytesCode),
+                string(abi.encodePacked(0x41a5881c17185383e19Df6FA4EC158a6F4851A69)),
+                string(abi.encodePacked(address(poolRegistryMock)))
+            )
+        );
+
+        // Deploy new contract using modified bytecode
+        fallbackConvexFrax = FallbackConvexFrax(
+            _deployBytecode(newBytesCode, abi.encode(address(this), rolesAuthority, address(curveStrategy)))
+        );
+    }
+
+    function _initializeBoosterConvexCurve() internal {
+        boosterConvexCurveMock.addPool(address(CRV3), address(0), address(0), address(new BaseRewardPoolMock()));
+        boosterConvexCurveMock.addPool(address(STETH_ETH), address(0), address(0), address(new BaseRewardPoolMock()));
+
+        poolRegistryMock.addPool(address(ALUSD_FRAXBP), STK_ALUSD_FRAXBP);
     }
 }
