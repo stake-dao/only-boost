0fd13da775b64b754c403a77b8314dd300277ffe
clement-ux <clemmoller@gmail.com>
2023-07-11 10:37:26 +0200

    feat(factory): import previous CurveVaultFactory contract

---
+++ src/interfaces/ICurveVault.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.20;
+
+interface ICurveVault {
+    function init(
+        address _token,
+        address _governance,
+        string memory name_,
+        string memory symbol_,
+        address _curveStrategy
+    ) external;
+
+    function setLiquidityGauge(address _liquidityGauge) external;
+
+    function setGovernance(address _governance) external;
+}
