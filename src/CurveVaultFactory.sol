6bd2a42bf06631df31ec4fc8f7b22ef37abaeb84
clement-ux <clemmoller@gmail.com>
2023-07-12 11:26:58 +0200

    docs(factory): add Natspec

--- src/CurveVaultFactory.sol
+++ src/CurveVaultFactory.sol
@@ -27,39 +27,77 @@ contract CurveVaultFactory {
     /// --- CONSTANTS
     ///////////////////////////////////////////////////////////////
 
+    /// @notice Curve DAO token address
     address public constant CRV = 0xD533a949740bb3306d119CC777fa900bA034cd52;
+
+    /// @notice Stake DAO token address
     address public constant SDT = 0x73968b9a57c6E53d41345FD57a6E6ae27d6CDB2F;
+
+    /// @notice Vote-escrow for SDT token address
     address public constant VESDT = 0x0C30476f66034E11782938DF8e4384970B6c9e8a;
+
+    /// @notice Stake DAO Liquid Locker for CRV token address
     address public constant LOCKER = 0x52f541764E6e90eeBc5c21Ff570De0e2D63766B6;
+
+    /// @notice Stake DAO  veboost contract address
     address public constant VEBOOST = 0xD67bdBefF01Fc492f1864E61756E5FBB3f173506;
+
+    /// @notice Stake DAO MultiSig contract address
     address public constant GOVERNANCE = 0xF930EBBd05eF8b25B1797b9b2109DDC9B0d43063;
+
+    /// @notice Claim rewards contract address
     address public constant CLAIM_REWARDS = 0xf30f23B7FB233172A41b32f82D263c33a0c9F8c2;
+
+    /// @notice Curve DAO Liquidity Gauge Controller contract address
     address public constant GAUGE_CONTROLLER = 0x2F50D538606Fa9EDD2B11E2446BEb18C9D5846bB;
 
     ////////////////////////////////////////////////////////////////
     /// --- VARIABLES
     ///////////////////////////////////////////////////////////////
 
+    /// @notice Curve Strategy contract
     CurveStrategy public curveStrategy;
+
+    /// @notice Stake DAO Curve Vault implementation contract
     address public vaultImpl = 0x9FDd0A0cfD98775565811E081d404309B23ea996;
+
+    /// @notice Stake DAO Curve Liquidity Gauge implementation contract
     address public gaugeImpl = 0x3Dc56D46F0Bd13655EfB29594a2e44534c453BF9;
+
+    /// @notice Stake DAO SDT distributor contract address
     address public sdtDistributor = 0x9C99dffC1De1AfF7E7C1F36fCdD49063A281e18C;
 
+    /// @notice Flag for setting all pids on fallback
     bool public setAllPids = true;
 
     ////////////////////////////////////////////////////////////////
     /// --- EVENTS
     ///////////////////////////////////////////////////////////////
 
+    /// @notice Event emitted when a new vault is deployed
+    /// @param proxy Address of the new deployed vault
+    /// @param lpToken Address of the corresponding LP token
+    /// @param impl Address of the implementation contract used as model
     event VaultDeployed(address proxy, address lpToken, address impl);
+
+    /// @notice Event emitted when a new gauge is deployed
+    /// @param proxy Address of the new deployed gauge
+    /// @param stakeToken Address of the corresponding stake token
+    /// @param impl Address of the implementation contract used as model
     event GaugeDeployed(address proxy, address stakeToken, address impl);
+
+    /// @notice Event emitted when `setAllPids` flag is toggled
+    /// @param status New status of the flag
     event SetAllPidsToggled(bool status);
 
     //////////////////////////////////////////////////////
     /// --- ERRORS
     //////////////////////////////////////////////////////
 
+    /// @notice Error emitted when the weight of the gauge is 0
     error INVALIDE_WEIGHT();
+
+    /// @notice Error emitted when the caller is not the governance
     error ONLY_GOVERNANCE();
 
     //////////////////////////////////////////////////////
