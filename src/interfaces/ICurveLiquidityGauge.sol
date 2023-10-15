78de38f5caa6c55ae04b0ea59a4f103389b716b8
Warren <99427649+kobe-eth@users.noreply.github.com>
2023-10-15 16:09:17 +0200

    chore: remove unused interfaces

--- src/interfaces/ICurveLiquidityGauge.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0
-pragma solidity 0.8.20;
-
-interface ICurveLiquidityGauge {
-    function lp_token() external view returns (address);
-}
--- src/interfaces/ISdToken.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0
-pragma solidity 0.8.20;
-
-interface ISdToken {
-    function setOperator(address _operator) external;
-    function operator() external returns (address);
-}
