be8e926a7f90777e7e8b01acafe8e0939cf0ee8f
Warren <99427649+kobe-eth@users.noreply.github.com>
2024-03-15 13:10:32 +0100

    fix: clean pool migration script

--- script/python/create_pool_migration_list.py
+++ script/python/create_pool_migration_list.py
@@ -1,15 +1,15 @@
-import os, json, requests
+import os
+import json
 from web3 import Web3
 from dotenv import load_dotenv
 
+# Load environment variables
 load_dotenv()
 
+# Contract addresses and other constants
 STRATEGY = "0x20F1d4Fed24073a9b9d388AfA2735Ac91f079ED6"
-NEW_STRATEGY = "0x20F1d4Fed24073a9b9d388AfA2735Ac91f079ED6"
 CONTROLLER = "0x2F50D538606Fa9EDD2B11E2446BEb18C9D5846bB"
 ZERO_ADDRESS = "0x0000000000000000000000000000000000000000"
-
-# Addresses that were used to test new strategies with small deposits.
 BLACKLIST = [
     "0x6ae7bf291028ccf52991bd020d2dc121b40bce2a",
     "0x54c9cb3ac40ef11c56565e8490e7c3b4b17582af",
@@ -18,66 +18,45 @@ BLACKLIST = [
     "0xb4d27b87a09ab76c47e342535a309a1176051481",
 ]
 
-with open("test/external/controller.json", "r") as f:
-    CONTROLLER_ABI = json.load(f)
+# Load contract ABIs
+def load_abi(file_name):
+    with open(f"test/external/{file_name}", "r") as f:
+        return json.load(f)
 
-
-with open("test/external/lgv5.json", "r") as f:
-    GAUGE_ABI = json.load(f)
-
-with open("test/external/strategy.json", "r") as f:
-    STRATEGY_ABI = json.load(f)
-
-with open("test/external/vault.json", "r") as f:
-    VAULT_ABI = json.load(f)
-
-with open("test/external/batch_set_strategy.json", "r") as f:
-    BATCH = json.load(f)
+CONTROLLER_ABI = load_abi("controller.json")
+GAUGE_ABI = load_abi("lgv5.json")
+STRATEGY_ABI = load_abi("strategy.json")
+VAULT_ABI = load_abi("vault.json")
 
 # Initialize Web3
 INFURA_URL = "https://mainnet.infura.io/v3/" + os.getenv("INFURA_KEY")
 w3 = Web3(Web3.HTTPProvider(INFURA_URL))
 
-strategy = w3.eth.contract(address=STRATEGY, abi=STRATEGY_ABI)
+# Set up contracts
 controller = w3.eth.contract(address=CONTROLLER, abi=CONTROLLER_ABI)
-
+strategy = w3.eth.contract(address=STRATEGY, abi=STRATEGY_ABI)
 
 def generate_pool_list():
     pool_length = controller.functions.n_gauges().call()
-
     pool_list = []
 
     for i in range(pool_length):
         gauge = controller.functions.gauges(i).call()
         multi_gauge = strategy.functions.multiGauges(gauge).call()
+        if multi_gauge == ZERO_ADDRESS:
+            continue
 
-        if multi_gauge != ZERO_ADDRESS:
-            _mg = w3.eth.contract(address=multi_gauge, abi=GAUGE_ABI)
-            ts = _mg.functions.totalSupply().call()
+        _mg = w3.eth.contract(address=multi_gauge, abi=GAUGE_ABI)
+        total_supply = _mg.functions.totalSupply().call()
+        balance_of_blacklist = sum(_mg.functions.balanceOf(w3.toChecksumAddress(b)).call() for b in BLACKLIST)
 
-            balanceOf = 0
-            for b in BLACKLIST:
-                b = w3.to_checksum_address(b)
-                balanceOf += _mg.functions.balanceOf(b).call()
+        if total_supply > 0 and total_supply != balance_of_blacklist:
+            vault = _mg.functions.staking_token().call()
+            pool_list.append({"gauge": gauge, "vault": vault, "rewardDistributor": multi_gauge})
 
-            if ts > 0 and balanceOf > 0 and ts == balanceOf:
-                print("Blacklisted: ", ts, balanceOf, ts == balanceOf, multi_gauge, i)
-                continue
-
-            if ts > 0:
-                vault = _mg.functions.staking_token().call()
-
-                pool_list.append(
-                    {"gauge": gauge, "vault": vault, "rewardDistributor": multi_gauge}
-                )
-
-        # Dump to JSON
-    with open("script/python/pools_migration.json", "w") as f:
+    # Dump the list to JSON
+    with open("pools_migration.json", "w") as f:
         json.dump(pool_list, f, indent=4)
 
-
-def main():
-    return generate_pool_list()
-
-
-__name__ == "__main__" and main()
+if __name__ == "__main__":
+    generate_pool_list()
