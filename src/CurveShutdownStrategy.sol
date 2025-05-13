a234f67fd8cdb619548b40309b2c42a4acac5ce8
Warren <99427649+kobe-eth@users.noreply.github.com>
2025-05-13 11:10:02 +0200

    feat: add protected gauge mapping wip

--- src/CurveShutdownStrategy.sol
+++ src/CurveShutdownStrategy.sol
@@ -53,6 +53,9 @@ contract CurveShutdownStrategy is CRVStrategy {
     /// @notice Mapping of shutdown gauges.
     mapping(address => bool) public isShutdown;
 
+    /// @notice Mapping of protected gauges.
+    mapping(address => bool) public protectedGauges;
+
     /// @notice Error thrown when a shutdown gauge is harvested.
     error SHUTDOWN();
 
@@ -73,6 +76,11 @@ contract CurveShutdownStrategy is CRVStrategy {
 
         /// Harvest as usual.
         super.harvest(asset, false, true, true);
+
+        /// Don't shutdown protected gauges.
+        if (protectedGauges[gauge]) {
+            return;
+        }
 
         /// 1. Get the vault address.
         address rewardDistributor = rewardDistributors[gauge];
@@ -108,5 +116,21 @@ contract CurveShutdownStrategy is CRVStrategy {
         if (isShutdown[gauge]) revert SHUTDOWN();
 
         super._withdraw(asset, amount);
+    }
+
+    /// @notice Set the protected gauges.
+    /// @param _gauges The gauges to set as protected.
+    function setProtectedGauges(address[] calldata _gauges) external onlyGovernance {
+        for (uint256 i = 0; i < _gauges.length; i++) {
+            protectedGauges[_gauges[i]] = true;
+        }
+    }
+
+    /// @notice Unset the protected gauges.
+    /// @param _gauges The gauges to unset as protected.
+    function unsetProtectedGauges(address[] calldata _gauges) external onlyGovernance {
+        for (uint256 i = 0; i < _gauges.length; i++) {
+            protectedGauges[_gauges[i]] = false;
+        }
     }
 }
--- src/YearnShutdownStrategy.sol
+++ src/YearnShutdownStrategy.sol
@@ -53,6 +53,9 @@ contract YearnShutdownStrategy is YearnStrategy {
     /// @notice Mapping of shutdown gauges.
     mapping(address => bool) public isShutdown;
 
+    /// @notice Mapping of protected gauges.
+    mapping(address => bool) public protectedGauges;
+
     /// @notice Error thrown when a shutdown gauge is harvested.
     error SHUTDOWN();
 
@@ -68,6 +71,11 @@ contract YearnShutdownStrategy is YearnStrategy {
 
         /// Harvest as usual.
         super.harvest(asset, false, true);
+
+        /// Don't shutdown protected gauges.
+        if (protectedGauges[gauge]) {
+            return;
+        }
 
         /// 1. Get the vault address.
         address rewardDistributor = rewardDistributors[gauge];
@@ -96,5 +104,21 @@ contract YearnShutdownStrategy is YearnStrategy {
         if (isShutdown[gauge]) revert SHUTDOWN();
 
         super._withdraw(asset, amount);
+    }
+
+    /// @notice Set the protected gauges.
+    /// @param _gauges The gauges to set as protected.
+    function setProtectedGauges(address[] calldata _gauges) external onlyGovernance {
+        for (uint256 i = 0; i < _gauges.length; i++) {
+            protectedGauges[_gauges[i]] = true;
+        }
+    }
+
+    /// @notice Unset the protected gauges.
+    /// @param _gauges The gauges to unset as protected.
+    function unsetProtectedGauges(address[] calldata _gauges) external onlyGovernance {
+        for (uint256 i = 0; i < _gauges.length; i++) {
+            protectedGauges[_gauges[i]] = false;
+        }
     }
 }
